1991c71d13d3747f94bd3f98b923d169fffbcfc4
吉本みゆき <watagashi1213@gmail.com>
2025-01-24 14:22:50 +0900

    ログイン機能の実装 #5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user
+      redirect_to login_path, notice: "アカウントが作成されました。ログインしてください！"
     else
       render 'new'
     end
