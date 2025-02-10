3a914ff4b95ca8975d2af8de67ad3968b61d8a88
吉本みゆき <watagashi1213@gmail.com>
2025-02-10 21:56:15 +0900

    使用期限を年月で登録

--- app/models/contact_lens.rb
+++ app/models/contact_lens.rb
@@ -1,9 +1,21 @@
 class ContactLens < ApplicationRecord
   self.table_name = 'contact_lenses'
+  before_save :set_expiration_date_to_beginning_of_month
 
   mount_uploader :image, ImageUploader
 
   private
+
+  def set_expiration_date_to_beginning_of_month
+  return unless expiration_date_before_type_cast.present?
+    
+    if expiration_date_before_type_cast.is_a?(String)
+      year, month = expiration_date_before_type_cast.split('-').map(&:to_i)
+      self.expiration_date = Date.new(year, month, 1)
+    end
+  rescue Date::Error
+    errors.add(:expiration_date, "は有効な日付ではありません")
+  end
 
   def image_size_validation
     if image.present? && image.size > 5.megabytes
