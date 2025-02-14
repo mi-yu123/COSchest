ac039c806eaac99e3964a4d00cc021637741e6d2
吉本みゆき <watagashi1213@gmail.com>
2025-02-14 11:26:52 +0900

    ActionTextの導入

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,58 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
     @articles = Article.all
     # プロフィールを作成後にコメントアウトを外す
     # @articles = Article.includes(:user)
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = current_user.articles.build(article_params)
+    if @article.save
+      redirect_to @article, notice: "記事を作成しました。"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice: "記事を更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path, notice: "記事を削除しました。"
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def authenticate_user!
+    redirect_to new_user_session_path, alert: "ログインしてください。"
+    unless user_signed_in?
+    end
   end
 end
