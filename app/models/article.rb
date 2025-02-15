8a6d09051e1270cb7ddbce010294d1dca38d9dfe
吉本みゆき <watagashi1213@gmail.com>
2025-02-15 18:37:19 +0900

    記事の文字数制限

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_rich_text :content
+
+  validates :title, presence: true, length: { maximum: 100, message: "は100文字以内で入力してください" }
+  validates :content, presence: true, length: { maximum: 10000, message: "は10000文字以内で入力してください" }
 end
