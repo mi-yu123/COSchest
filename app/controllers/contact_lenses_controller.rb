28cde11ad3a3c318f814dd275299be8a8f972d40
吉本みゆき <watagashi1213@gmail.com>
2025-02-16 15:45:29 +0900

    カラコン管理権限

--- app/controllers/contact_lenses_controller.rb
+++ app/controllers/contact_lenses_controller.rb
@@ -1,9 +1,10 @@
 class ContactLensesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_contact_lens, only: [:edit, :update, :destroy]
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def index
-    @contact_lenses = ContactLens.all
+    @contact_lenses = current_user.contact_lenses
   end
 
   def new
@@ -12,6 +13,7 @@ class ContactLensesController < ApplicationController
 
   def create
     @contact_lens = ContactLens.new(contact_lens_params)
+    @contact_lens.user = current_user
     if @contact_lens.save
       redirect_to contact_lenses_path, notice: "カラコンが登録されました"
     else
@@ -44,5 +46,12 @@ class ContactLensesController < ApplicationController
 
   def contact_lens_params
     params.require(:contact_lens).permit(:name, :image, :expiration_date, :quantity, :memo)
+  end
+
+  def ensure_correct_user
+    @contact_lens = ContactLens.find(params[:id])
+    unless @contact_lens.user == current_user
+      redirect_to contact_lenses_path, alert: "権限がありません"
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_16_062906) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_16_063547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_16_062906) do
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_contact_lenses_on_user_id"
   end
 
   create_table "costumes", force: :cascade do |t|
@@ -110,6 +112,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_16_062906) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "contact_lenses", "users"
   add_foreign_key "costumes", "users"
   add_foreign_key "wigs", "users"
 end
