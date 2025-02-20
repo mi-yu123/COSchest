efe153169c380cfe73d1fb5a4c15565099f01365
吉本みゆき <watagashi1213@gmail.com>
2025-02-20 17:03:37 +0900

    taksモデル生成

---
+++ db/migrate/20250220080139_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[7.2]
+  def change
+    create_table :tasks do |t|
+      t.string :title, null: false
+      t.string :description
+      t.datetime :due_date
+      t.boolean :completed, default: false
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
