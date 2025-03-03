13213990a278b413736b1e2b2cb144602c1d122d
吉本みゆき <watagashi1213@gmail.com>
2025-03-03 15:23:15 +0900

    バリデーションとビューの調整

--- app/controllers/packing_lists_controller.rb
+++ app/controllers/packing_lists_controller.rb
@@ -1,5 +1,7 @@
 class PackingListsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user_items, only: %i[new create edit update]
+  before_action :set_packing_list, only: %i[edit update destroy]
 
   def index
     @packing_lists = current_user.packing_lists.includes(:itemable)
@@ -7,53 +9,47 @@ class PackingListsController < ApplicationController
 
   def new
     @packing_list = current_user.packing_lists.new
-    @costumes = current_user.costumes
-    @wigs = current_user.wigs
-    @contact_lenses = current_user.contact_lenses
   end
 
   def create
     @packing_list = current_user.packing_lists.build(packing_list_params)
   
     if @packing_list.save
-      redirect_to packing_lists_path, flash: { success: '登録しました' }
+      redirect_to packing_lists_path, flash: { message: '登録しました' }
     else
-      @costumes = current_user.costumes
-      @wigs = current_user.wigs
-      @contact_lenses = current_user.contact_lenses
       render :new, status: :unprocessable_entity
     end
   end
 
   def edit
-    @packing_list = current_user.packing_lists.find(params[:id])
-    @costumes = current_user.costumes
-    @wigs = current_user.wigs
-    @contact_lenses = current_user.contact_lenses
   end
 
   def update
-    @packing_list = current_user.packing_lists.find(params[:id])
-
     if @packing_list.update(packing_list_params)
-      redirect_to packing_lists_path, flash: { success: '更新しました' }
+      redirect_to packing_lists_path, flash: { message: '更新しました' }
     else
-      @costumes = current_user.costumes
-      @wigs = current_user.wigs
-      @contact_lenses = current_user.contact_lenses
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @packing_list = current_user.packing_lists.find(params[:id])
     @packing_list.destroy
-    redirect_to packing_lists_path, flash: { success: '削除しました' }
+    redirect_to packing_lists_path, flash: { message: '削除しました' }
   end
 
   private
 
+  def set_user_items
+    @costumes = current_user.costumes
+    @wigs = current_user.wigs
+    @contact_lenses = current_user.contact_lenses
+  end
+
+  def set_packing_list
+    @packing_list = current_user.packing_lists.find(params[:id])
+  end
+
   def packing_list_params
-    params.require(:packing_list).permit(:item, :itemable_type, :itemable_id)
+    params.require(:packing_list).permit(:item, :packed, :itemable_type, :itemable_id)
   end
 end
