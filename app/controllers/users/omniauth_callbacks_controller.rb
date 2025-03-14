255f2e245d5e4a34514778a6073bfdbd8beaec79
吉本みゆき <watagashi1213@gmail.com>
2025-03-14 22:54:26 +0900

    フラッシュメッセージ設置

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,6 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
+      flash[:notice] = "ログインしました"
       sign_in_and_redirect @user, event: :authentication
     else
       redirect_to new_user_registration_url
