b8d11c490a6dd1f76e2496a84a8582cf80d077c0
吉本みゆき <watagashi1213@gmail.com>
2025-03-30 12:29:14 +0900

    ActionText修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.variant_processor = :mini_magick
   config.active_storage.service = :amazon
+  config.active_storage.resolve_model_to_route = :rails_storage_proxy
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
