ae3db31ecb3dc511523a1174f105ac1928d942be
吉本みゆき <watagashi1213@gmail.com>
2025-04-13 12:01:54 +0900

    関連モデル検索対策

--- app/models/wig.rb
+++ app/models/wig.rb
@@ -12,6 +12,10 @@ class Wig < ApplicationRecord
     %w[character_name status]
   end
 
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
   def image_content_type
     if image.attached? && !image.content_type.in?(%w[image/jpeg image/png])
       errors.add(:image, 'ファイルの形式はJPEGまたはPNGである必要があります。')
