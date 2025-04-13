6a540022df38c16ed5a135b927762bfbda90e6ad
吉本みゆき <watagashi1213@gmail.com>
2025-04-13 17:29:44 +0900

    記事の検索機能

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,6 +7,14 @@ class Article < ApplicationRecord
     attachable.variant :large, resize_to_limit: [800, 800]
   end
 
+  def self.ransackable_attributes(auth_object = nil)
+    %w[title user_id]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
   validates :title, presence: true, length: { maximum: 100, message: "は100文字以内で入力してください" }
   validates :content, presence: true, length: { maximum: 10000, message: "は10000文字以内で入力してください" }
 end
