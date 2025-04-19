a07aa9a8f4a4b241bd086c7e01a166f9d470002e
吉本みゆき <watagashi1213@gmail.com>
2025-04-19 12:28:10 +0900

    セキュリティ設定

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,13 @@ module Myapp
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.action_dispatch.default_headers = {
+      "Content-Security-Policy" => "frame-ancestors 'self' https://docs.google.com/; frame-src 'self' https://docs.google.com/",
+      "X-Content-Type-Options" => 'nosniff",
+      "X-XSS-Protection" => "1; mode=block",
+      "Referrer-Policy" => "strict-origin-when-cross-origin"
+    }
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
