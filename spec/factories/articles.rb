4b990edb3e53fc69e0a4d6ae6504dd74f28f4423
吉本みゆき <watagashi1213@gmail.com>
2025-04-22 14:55:53 +0900

    articleモデルテストと修正

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,21 @@
 FactoryBot.define do
   factory :article do
     title { Faker::Lorem.sentence }
-    content { Faker::Lorem.paragraph }
+    
+    after(:build) do |article|
+      article.content = ActionText::Content.new(Faker::Lorem.paragraph(sentence_count: 3))
+    end
+
     association :user
+
+    trait :with_featured_image do
+      after(:build) do |article|
+        article.featured_image.attach(
+          io: File.open(Rails.root.join('spec', 'fixtures', 'files', 'test.jpeg')),
+          filename: 'test.jpeg',
+          content_type: 'image/jpeg'
+        )
+      end
+    end
   end
 end
