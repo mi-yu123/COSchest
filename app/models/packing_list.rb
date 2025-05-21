da342a0b7246d02a52f3d00f977a1c1f1acc79ca
吉本みゆき <watagashi1213@gmail.com>
2025-05-21 12:45:02 +0900

    持ち物のエラーメッセージを修正

--- app/models/packing_list.rb
+++ app/models/packing_list.rb
@@ -2,12 +2,14 @@ class PackingList < ApplicationRecord
   belongs_to :user
   belongs_to :itemable, polymorphic: true, optional: true
 
-  validates :item, presence: true, unless: :itemable_present?
+  validate :item_or_itemable_present
   validates :item, length: { maximum: 50, message: 'は50文字以内で入力してください' }, if: :item?
 
   private
 
-  def itemable_present?
-    itemable.present? && itemable_id.present?
+  def item_or_itemable_present
+    if item.blank? && itemable_id.blank?
+      errors.add(:base, 'アイテム名を入力、または関連アイテムを選択してください')
+    end
   end
 end
